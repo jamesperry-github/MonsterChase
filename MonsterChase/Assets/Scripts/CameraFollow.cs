1662d3c8d48f700d9ac170d696c820d168a91cb0
jamesperry-github <crimsonperry.github@gmail.com>
2022-11-11 10:18:29 -0800

    added camera follow and border collisions;

--- MonsterChase/Assets/Scripts/CameraFollow.cs
+++ MonsterChase/Assets/Scripts/CameraFollow.cs
@@ -6,6 +6,13 @@ public class CameraFollow : MonoBehaviour
 {
     private Transform player;
     private Vector3 tempPos;
+    private float minX = -20f;
+    private float maxX = 20f;
+
+    private void Awake()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -13,8 +20,18 @@ public class CameraFollow : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        tempPos = transform.position; // current position of camera
+        tempPos.x = player.position.x; // current horizontal position of player
+        if (tempPos.x < minX)
+        {
+            tempPos.x = minX;
+        }
+        if (tempPos.x > maxX)
+        {
+            tempPos.x = maxX;
+        }
+        transform.position = tempPos;
     }
 }
